bcd08f7b9a06647b224158e4d9aa2e1e091e7c7f
Abu_Roslan <abdulrasaqjamiu767@gmail.com>
2026-04-16 10:15:50 +0100

    fourth

--- connectdb.php
+++ connectdb.php
@@ -7,5 +7,10 @@ $dbname= 'todo_app';
 
 
 $connect= mysqli_connect($server,$username,$password,$dbname);
+if(!$connect){
+    http_response_code(500);
+    die(json_encode(["status"=>"error","message"=>"Database connection failed"]));
+}
+mysqli_set_charset($connect,'utf8mb4');
 
 ?>
